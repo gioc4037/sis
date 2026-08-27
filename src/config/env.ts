07ffb3c3f8a82104e264731c353e09893849f337
samsheybk <samirvasquez0104@gmail.com>
2026-08-26 20:51:06 -0400

    'fix-include-env-ts-for-vercel-build'

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  SUPABASE_URL: 'https://viopzsgthvoguerkxxji.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpb3B6c2d0aHZvZ3Vlcmt4eGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NzcyNDAsImV4cCI6MjEwMzM1MzI0MH0.FCPKd4ZHfxq9ZStf0y4ZQqWkszm3FM_6BAtIth6Ke20',
+};
